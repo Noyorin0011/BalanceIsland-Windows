dc973bb46e52dcf3a84ecb94bd666c6a587c4553
Noyorin_ <63433291+Noyorin0011@users.noreply.github.com>
2026-08-28 12:56:14 +0800

    fix: disambiguate WPF edit dialog types

--- src/BalanceIsland.Windows/AccountEditWindow.xaml.cs
+++ src/BalanceIsland.Windows/AccountEditWindow.xaml.cs
@@ -1,5 +1,8 @@
 using System.Globalization;
 using System.Windows;
+using MessageBox = System.Windows.MessageBox;
+using MessageBoxButton = System.Windows.MessageBoxButton;
+using MessageBoxImage = System.Windows.MessageBoxImage;
 
 namespace BalanceIsland.Windows;
 
